2eec2667999cf17782bcce4346f3badd71cd51f9
LeFoenk <jonas.knoefel@googlemail.com>
2024-12-01 13:06:17 +0100

    day-01 part 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = "src/input.txt";
+
+    let content = fs::read_to_string(file_path)?;
+
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    for line in content.split('\n') {
+        match line.split_once(' ') {
+            Some((num_left, num_right)) => {
+                left.push(num_left.parse::<i32>()?);
+                right.push(num_right.trim_start().parse::<i32>()?);
+            }
+            None => {}
+        }
+    }
+
+    left.sort();
+    right.sort();
+
+    let difference: i32 = left
+        .iter()
+        .zip(right.iter())
+        .map(|(a, b)| (a - b).abs())
+        .sum();
+
+    println!("Difference: {}", difference);
+
+    Ok(())
 }
